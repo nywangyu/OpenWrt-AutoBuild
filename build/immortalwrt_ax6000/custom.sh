1209b7cf90e378009ae633e6391e5df458918825
nywangyu <lbxshu@163.com>
2026-03-04 13:34:56 +0800

    Update custom.sh

--- build/immortalwrt_ax6000/custom.sh
+++ build/immortalwrt_ax6000/custom.sh
@@ -149,10 +149,8 @@ cat >> .config <<EOF
 CONFIG_TARGET_mediatek=y
 CONFIG_TARGET_mediatek_filogic=y
 CONFIG_TARGET_mediatek_filogic_DEVICE_jdcloud_re-cp-03=y
-EOF
 
 # LuCI插件
-cat >> .config <<EOF
 CONFIG_PACKAGE_luci-app-argon-config=y
 CONFIG_PACKAGE_luci-app-ddns=y
 CONFIG_PACKAGE_luci-app-dockerman=y
@@ -169,10 +167,8 @@ CONFIG_PACKAGE_luci-app-store=y
 CONFIG_PACKAGE_luci-app-ttyd=y
 CONFIG_PACKAGE_luci-app-turboacc-mtk=y
 CONFIG_PACKAGE_luci-app-upnp=y
-EOF
 
 # LuCI主题:
-cat >> .config <<EOF
 CONFIG_PACKAGE_luci-theme-argon=y
 CONFIG_PACKAGE_luci-theme-edge=n
 EOF
