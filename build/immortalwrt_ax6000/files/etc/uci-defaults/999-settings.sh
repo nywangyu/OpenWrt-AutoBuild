74f155dcf650e4eca91b4994709544c0e26d988c
nywangyu <lbxshu@163.com>
2026-03-04 16:57:33 +0800

    Update 999-settings.sh

--- build/immortalwrt_ax6000/files/etc/uci-defaults/999-settings.sh
+++ build/immortalwrt_ax6000/files/etc/uci-defaults/999-settings.sh
@@ -2,13 +2,7 @@
 #!/bin/bash
 
 # 软件源设置
-cat << EOF > "/etc/opkg/distfeeds.conf"
-src/gz openwrt_base https://downloads.immortalwrt.org/releases/24.10.5/packages/aarch64_cortex-a53/base/
-src/gz openwrt_luci https://downloads.immortalwrt.org/releases/24.10.5/packages/aarch64_cortex-a53/luci/
-src/gz openwrt_packages https://downloads.immortalwrt.org/releases/24.10.5/packages/aarch64_cortex-a53/packages
-src/gz openwrt_routing https://downloads.immortalwrt.org/releases/24.10.5/packages/aarch64_cortex-a53/routing
-src/gz openwrt_telephony https://downloads.immortalwrt.org/releases/24.10.5/packages/aarch64_cortex-a53/telephony
-EOF
+
 
 # 指示灯定义
 if ! grep -q "option name 'LAN'" /etc/config/system; then
