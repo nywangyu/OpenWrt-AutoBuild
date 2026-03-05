420cca9aa7d9e21f6661675b48e9d41ddf790129
nywangyu <lbxshu@163.com>
2026-03-05 10:14:10 +0800

    Update custom.sh

--- build/Lean_ax6000/custom.sh
+++ build/Lean_ax6000/custom.sh
@@ -41,7 +41,6 @@ echo "uci set luci.main.mediaurlbase=/luci-static/argon" >> $ZZZ                
 # ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● #
 
 BUILDTIME=$(TZ=UTC-8 date "+%Y.%m.%d") && sed -i "s/\(_('Firmware Version'), *\)/\1 ('ONE build $BUILDTIME @ ') + /" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js               # 增加自己个性名称
-# wget -q https://raw.githubusercontent.com/immortalwrt/immortalwrt/refs/heads/main/package/firmware/ath11k-firmware/Makefile -O package/firmware/ath11k-firmware/Makefile --no-check-certificate               # 更新 ath11k-firmware Makefile
 
 
 # ●●●●●●●●●●●●●●●●●●●●●●●●定制部分●●●●●●●●●●●●●●●●●●●●●●●● #
